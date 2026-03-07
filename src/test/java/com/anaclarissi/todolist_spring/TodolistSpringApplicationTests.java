6a9c56d71b263ab935d1617b84a57caefd2340d5
Ana Clarissi <anaclarissicruz@gmail.com>
2026-03-07 15:27:38 -0300

    created project

---
+++ src/test/java/com/anaclarissi/todolist_spring/TodolistSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anaclarissi.todolist_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
